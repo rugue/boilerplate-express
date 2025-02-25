412c3efdfae07e71cb657e55ff714bf1f44d2b20
Enehizena Osarugue <nehirugue@gmail.com>
2025-02-25 14:20:20 +0100

    feat(myApp.js): enhance JSON endpoint to support dynamic message styling based on environment variable

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require("express");
 let app = express();
+require("dotenv").config();
 
 console.log("Hello World");
 
@@ -13,8 +14,16 @@ app.get("/", (req, res) => {
 
 app.use("/public", express.static(__dirname + "/public"));
 
+// app.get("/json", (req, res) => {
+//   res.json({ message: "Hello json" });
+// });
+
 app.get("/json", (req, res) => {
-  res.json({ message: "Hello json" });
+    let message = "Hello json";
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        message = message.toUpperCase();
+    }
+    res.json({"message": message});
 });
 
 module.exports = app;
