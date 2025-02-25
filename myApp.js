ba25b1ae24ac00d6a0d169e4f06cada5d0ea5211
Enehizena Osarugue <nehirugue@gmail.com>
2025-02-25 12:48:33 +0100

    fix(myApp.js): update root route to serve HTML file instead of plain text

--- myApp.js
+++ myApp.js
@@ -1,38 +1,14 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-
-
 
 console.log("Hello World");
 
+// app.get("/", (req, res) => {
+//   res.send("Hello Express");
+// });
+
 app.get("/", (req, res) => {
-    res.send("Hello Express");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
